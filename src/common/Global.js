01dfb5634bd59d4c577e40fb60bb624189742944
lihongjun <lihongjun@qiyi.com>
2017-11-30 16:07:52 +0800

    empty page

--- src/common/Global.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kerwinliu on 2017/11/9.
- */
-import QYRequest from './QYRequest'
-
-global.QYRequest = QYRequest
-QYRequest.setMockFlag(true)
--- src/main.js
+++ src/main.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native'
-import { StackNavigator } from '@iqiyi/rn-navigation'
+import React, {Component} from 'react';
+import {AppRegistry, View, Text} from 'react-native'
 
-// 项目路由
-import routeConfig from './routes';
-// 加载全局变量
-import './common/Global'
 
 export default class App extends Component {
-  navigator = null
 
-  render() {
-    // 加载首屏页面，如果是从第三方跳转, url 统一带上 pageName 参数
-    const routeName = this.props.pageName || 'DemoPage'
-    // 必须配置首屏路由
-    const initialRoute = {
-      routeName
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: 'red'}}>
+            </View>
+        )
     }
-
-    const AppNavigator = StackNavigator(routeConfig, initialRoute)
-    return (
-      <AppNavigator ref={nav => this.navigator = nav} />
-    )
-  }
 }
 
 AppRegistry.registerComponent('LightNight', () => App);
